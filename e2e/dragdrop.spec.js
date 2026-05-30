67ecbdd311b56292140219c6cfb54b047ecb7f26
Muhit Ram <muhitram26@gmail.com>
2026-05-30 15:43:26 +0530

    Webtable Changes

--- e2e/dragdrop.spec.js
+++ e2e/dragdrop.spec.js
@@ -3,10 +3,10 @@ import {test,expect} from '@playwright/test';
 test('Basic lines',async({page})=>{
 
     await page.goto('https://the-internet.herokuapp.com/drag_and_drop')
-    const source1=await page.locator('#column-a');
+    const source12=await page.locator('#column-a');
     const destination =await page.locator('#column-b');
 
-    await source1.dragTo(destination);
+    await source12.dragTo(destination);
 
     await page.waitForTimeout(3000);
 })
